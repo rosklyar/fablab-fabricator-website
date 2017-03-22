345163862b105fdaec7f9138284e7514c2c4a421
Fedor Bobylev <fedorbobylev@gmail.com>
2017-03-22 10:37:11 +0200

    Added browserSync to ease up the job

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-
 'use strict';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require("browser-sync").create();
 var minify = require("gulp-minify-css");
 
 gulp.task('default', ['watch', 'build-css']);
@@ -12,9 +12,17 @@ gulp.task('build-css', () => {
     return gulp.src('./styles/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(minify())
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('.'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./styles/**/*.scss', ['build-css']);
+gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch('./styles/**/*.scss', ['build-css']);
+    gulp.watch('./**/*.html', ['build-css']).on('change', browserSync.reload);
 });
